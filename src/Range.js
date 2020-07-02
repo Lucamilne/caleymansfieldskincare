7477790557c4e3bf1a6ac0cfdf979e80d16feaed
Luca Milne <lucamilne@googlemail.com>
2020-07-02 18:34:24 +0100

    wardrobes added

--- src/Range.js
+++ src/Range.js
@@ -1,15 +1,16 @@
 import kitchens from "./data/kitchens.json"
+import bedrooms from "./data/bedrooms.json"
 import React, { useState } from "react";
 import { Carousel, Row, Col, Container } from "react-bootstrap";
 
-const Kitchens = (props) => {
-    const [index, setIndex] = useState(0);
+//no props as of yet
+const Range = (props) => {
 
-    const handleSelect = (selectedIndex, e) => {
-        setIndex(selectedIndex);
-    };
 
-    const kitchenSlides = kitchens.map(item => {
+    const kitchenSlides = kitchens.map(item => returnCarouselSlide(item))
+    const bedroomSlides = bedrooms.map(item => returnCarouselSlide(item))
+
+    function returnCarouselSlide(item) {
         return (
             <Carousel.Item key={item.name}>
                 <img
@@ -22,27 +23,41 @@ const Kitchens = (props) => {
                 </Carousel.Caption>
             </Carousel.Item>
         )
-    })
+    }
 
 
     return (
         <Container fluid className="range">
             <Row>
-                <Col md>
-                    <Carousel activeIndex={index} onSelect={handleSelect}>
-                        {kitchenSlides}
-                    </Carousel>
-                </Col>
-                <Col md className="range__text d-flex flex-column justify-content-center">
+                <Col lg className="range__text d-flex flex-column justify-content-center">
                     <Container>
                         <h1>Kitchens</h1>
                         <p>From contemporary design to retro suggestions, from minimal linearity to classic elegance, the modern and classic kitchens by Gicinque are designed to meet any need for space and style, always ensuring quality, features, attention to detail and durability.</p>
                     </Container>
                 </Col>
+                <Col lg>
+                    <Carousel interval={null}>
+                        {kitchenSlides}
+                    </Carousel>
+                </Col>
             </Row>
-
+            <Row className="flex-lg-row-reverse">
+                <Col lg className="range__text d-flex flex-column justify-content-center">
+                    <Container>
+                        <h1>Bedrooms</h1>
+                        <p>A vast range of carefully designed solutions, a perfect match with every style of furnishing.
+                        Additionally, we offer a shelving system with optional fittings to create the perfect walk-in wardrobe.
+                        </p>
+                    </Container>
+                </Col>
+                <Col lg>
+                    <Carousel interval={null}>
+                        {bedroomSlides}
+                    </Carousel>
+                </Col>
+            </Row>
         </Container>
     )
 }
 
-export default Kitchens;
+export default Range;
