2ace69ef0d34c525c4102dbefbae7fc254bb425b
Luca Milne <lucamilne@googlemail.com>
2020-07-03 17:44:11 +0100

    Updates

---
+++ precache-manifest.368005438defb3127456f9f8d98c8534.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e591c63ebed9a077a8d0e20250dd2b0",
+    "url": "/Kitchenism/index.html"
+  },
+  {
+    "revision": "15e116a53802142935d4",
+    "url": "/Kitchenism/static/css/main.eb5a8fec.chunk.css"
+  },
+  {
+    "revision": "b2b67868815313f94709",
+    "url": "/Kitchenism/static/js/2.f5d486c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Kitchenism/static/js/2.f5d486c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e116a53802142935d4",
+    "url": "/Kitchenism/static/js/main.096a2623.chunk.js"
+  },
+  {
+    "revision": "87ec1acc6903e04a65f7",
+    "url": "/Kitchenism/static/js/runtime-main.f0f17471.js"
+  },
+  {
+    "revision": "d233e7b33aac6ef4dc54d1fa721bd5e1",
+    "url": "/Kitchenism/static/media/Interior-2.d233e7b3.jpg"
+  }
+]);
