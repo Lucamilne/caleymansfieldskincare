8fb8cfc1cba4fc5db7dec3791b0d6587f5df36c2
lucamilne <lucamilne@googlemail.com>
2020-08-24 14:03:51 +0100

    testimonials updated with styling

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,18 +3,23 @@ import { Navbar, Nav, Button } from "react-bootstrap";
 import Filter from "./Filter"
 
 class Navigation extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {isMenuOpen: false};
-    
+    constructor() {
+        super();
+        this.state = {
+            isMenuOpen: false,
+            address: "96 Marlow Bottom, SL7 3PH",
+            email: "caleymansfield@icloud.com",
+            number: "077976 962012"
+        };
+
         // This binding is necessary to make `this` work in the callback
         this.toggleMenu = this.toggleMenu.bind(this);
-      }
+    }
 
     toggleMenu() {
         this.setState(state => ({
             isMenuOpen: !state.isMenuOpen
-          }));
+        }));
     }
 
     render() {
@@ -22,7 +27,7 @@ class Navigation extends React.Component {
 
         return (
             <div>
-                <Filter className={this.state.isMenuOpen ? "d-block" : "d-none"}/>
+                <Filter className={this.state.isMenuOpen ? "d-block" : "d-none"} />
                 <div className={`nav__menu d-block d-lg-none ${isOpen}`}>
                     <Nav className="d-flex flex-column align-items-center justify-content-center">
                         <Nav.Link href="#about">About</Nav.Link>
@@ -53,9 +58,9 @@ class Navigation extends React.Component {
 
                     <div className="d-none d-lg-block nav__section">
                         <Nav className="flex-column">
-                            <Nav.Link><i className="fas fa-map-marker-alt"></i>96 Marlow Bottom, SL7 3PH</Nav.Link>
-                            <Nav.Link><i className="far fa-envelope"></i>caleymansfield@icloud.com</Nav.Link>
-                            <Nav.Link><i className="fas fa-phone-alt"></i>(0) 7976 962012</Nav.Link>
+                            <Nav.Link href="https://www.google.com/maps/place/96+Marlow+Bottom,+Marlow+SL7+3PH/" target="_blank"><i className="fas fa-map-marker-alt"></i>{this.state.address}</Nav.Link>
+                            <Nav.Link href={`mailto:${this.state.address}`}><i className="far fa-envelope"></i>{this.state.email}</Nav.Link>
+                            <Nav.Link href={`tel:${this.state.number}`}><i className="fas fa-phone-alt"></i>{this.state.number}</Nav.Link>
                         </Nav>
                     </div>
 
--- src/Testimonials.js
+++ src/Testimonials.js
@@ -4,35 +4,37 @@ import { Container, Carousel, CarouselItem } from "react-bootstrap"
 class Testimonials extends React.Component {
     render() {
         return (
-            <Container className="pl-0 pr-0">
+            <Container className="pl-0 pr-0" id="testimonials">
+                <h1 className="title">Testimonials</h1>
                 <Carousel interval={null} touch="true" nextIcon={<span className='fas fa-caret-right' />} prevIcon={<span className='fas fa-caret-left' />} className="carousel-dark">
                     <Carousel.Item className="slide">
                         <Carousel.Caption>
-                            <p>I can not recommend Caley enough.  I have always struggled with finding a skin routine which suited my skin, but since I have been going to Caley I have seen a massive transformation in my skin.  Caley is hugely knowledgeable and always offers great advice, in a friendly relaxed environment</p>
-                            <h6>Jean Sugg</h6>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="slide">
-                        <Carousel.Caption>
                             <p>
-                                Caley’s attention to detail is beyond compare and her enthusiasm is a joy to behold. It is such a treat to enjoy customer care of the highest standard. She is - quite simply - an expert in her field.
-                            </p>
-                            <h6>Mike Dillon</h6>
-                        </Carousel.Caption>
-                    </Carousel.Item>
-                    <Carousel.Item className="slide">
-                        <Carousel.Caption>
-                            <p>
+                                <i className="fas fa-quote-left mr-1"></i>
                                 I’m writing this review for Caley’s Signature Skin Revival treatment - As it’s so much better than any other facial that I have experienced. First the environment is very relaxing and just the right amount of attention to hygiene without being clinical. The facial I have is one that makes me feel calm and relaxed and yet I feel the real benefits of the different treatments for many weeks afterwards. I like all the varied applications and look forward to each aspect of the facial with Caley’s friendly approach it always seems to finish with me wishing I could stay for another hour or two!
+                                <i className="fas fa-quote-right ml-1"></i>
                             </p>
-                            <h6>Su Da Costa</h6>
+                            <h6 className="name">Su Da Costa</h6>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item className="slide">
                         <Carousel.Caption>
                             <p>
-                                I can’t recommend Caley highly enough. I’ve had issues with my skin since my early 20s and have tried every lotion and potion under the sun all in search of the holy grail of soft, blemish free, hydrated skin. I was recommended to Caley by a friend and admit to being skeptical but she really listened to my concerns and past history and set out a treatment plan to rebuild my skin barrier and get me on the way to my skin goal. Not only have I not had a blemish or skin reaction since taking her advice and using the recommended products but Caley is always checking in on how I’m getting on and offering advice as needed...even throughout the Covid crisis despite not being able to offer appointments she has been in regular contact to make sure all is well. She truly cares about you and your skin and has such a warm and lovely persona that she instantly puts you at ease. If you want a true skin professional then please don’t go anywhere else!                            </p>
-                            <h6>Gina Coster</h6>
+                                <i className="fas fa-quote-left mr-1"></i>
+                                I can’t recommend Caley highly enough. I’ve had issues with my skin since my early twenties and have tried every lotion under the sun all in search of the holy grail of soft, blemish-free, hydrated skin.  Not only have I not had a blemish or skin reaction since taking her advice but Caley is always checking in on how I’m getting on and offering advice as needed. even throughout the COVID crisis, despite not being able to offer appointments she has been in regular contact to make sure all is well. She truly cares about you and your skin and has such a warm and lovely persona that she instantly puts you at ease.
+                                <i className="fas fa-quote-right ml-1"></i>
+                            </p>
+                            <h6 className="name">Gina Coster</h6>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item className="slide">
+                        <Carousel.Caption>
+                            <p>
+                                <i className="fas fa-quote-left mr-1"></i>
+                                I can not recommend Caley enough. I have always struggled with finding a skin routine which suited my skin, but since I have been going to Caley I have seen a massive transformation in my skin. Caley is hugely knowledgeable and always offers great advice, in a friendly relaxed environment. Her attention to detail is beyond compare and her enthusiasm is a joy to behold. It is such a treat to enjoy customer care of the highest standard.
+                                <i className="fas fa-quote-right ml-1"></i>
+                            </p>
+                            <h6 className="name">Jean Sugg</h6>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
