271a82dfe26eca5738f6aa9e6b47f5a335f0f114
lucamilne <lucamilne@googlemail.com>
2020-08-24 14:51:49 +0100

    updated image

--- src/Hero.js
+++ src/Hero.js
@@ -6,8 +6,8 @@ class Hero extends React.Component {
     render() {
         return (
             <div className="hero position-relative">
-                {/* <img src={require("./images/1.jpg")} /> */}
-                <video width="100%" autoPlay muted loop>
+                <img src={require("./images/1.jpg")} className="d-sm-block d-md-none"/>
+                <video width="100%" autoPlay muted loop className="d-none d-md-block">
                     <source src={require("./images/routine2.mp4")} type="video/mp4" />
                 </video>
                 <div className="position-absolute d-flex align-items-end">
