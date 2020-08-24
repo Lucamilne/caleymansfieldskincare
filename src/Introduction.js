58554b7173349233d8bcb08e8c4007283c2ff7f0
lucamilne <lucamilne@googlemail.com>
2020-08-24 18:45:11 +0100

    introduction styling changed

--- src/Introduction.js
+++ src/Introduction.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { Container, Button } from "react-bootstrap"
+import { Container, Button, Row, Col } from "react-bootstrap"
 
 class Introduction extends React.Component {
     render() {
         return (
-            <Container className="introduction d-flex justify-content-center align-items-center">
-                <div className="introduction__wrapper d-flex flex-column align-items-center">
-                    <h5>Extraordinary experience in skincare</h5>
-                    <h1><span>welcome!</span>This is an introductory statement</h1>
+            <Container id="introduction" className="d-flex align-items-center">
+                <div className="introduction__wrapper">
+                    <Row>
+                        <Col>
+                            <h1>Get the best</h1>
+                        </Col>
+                    </Row>
+                    <Row className="last-row">
+                        <Col>
+                            <h1>out of your skin.</h1>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <p>Every single treatment we offer is tailored to your individual needs.</p>
+                        </Col>
+                    </Row>
+                    <Row className="last-row">
+                        <Col>
+                            <p>No two skin complexions are the same.</p>
+                        </Col>
+                    </Row>
                     <Button variant="outline-primary">Read More</Button>
                 </div>
             </Container>
