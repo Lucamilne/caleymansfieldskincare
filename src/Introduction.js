ca42876f8b4406686e11b63a8ad544b6a7e67721
lucamilne <lucamilne@googlemail.com>
2020-08-26 14:57:46 +0100

    bug check

--- src/Introduction.js
+++ src/Introduction.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Button, Row, Col } from "react-bootstrap"
+import { Container, Image, Row, Col } from "react-bootstrap"
 import { useInView } from 'react-intersection-observer'
 import { motion } from 'framer-motion'
 
@@ -12,28 +12,30 @@ const Introduction = () => {
     return (
         <Container id="introduction" className="d-flex align-items-center my-5">
             <motion.div ref={ref} animate={inView ? { opacity: [0, 1] } : { opacity: 0 }} transition={{ duration: 0.5 }}>
-                <Row>
-                    <Col>
-                        <h1>Get the best</h1>
-                    </Col>
-                </Row>
-                <Row className="mb-3">
-                    <Col>
-                        <h1>out of your skin.</h1>
-                    </Col>
-                </Row>
-                <div className="paragraph pl-3 mb-3">
+                <Col>
                     <Row>
                         <Col>
-                            <p>Every single treatment we offer is tailored to your individual needs.</p>
+                            <h1>Get the best</h1>
                         </Col>
                     </Row>
-                    <Row>
+                    <Row className="mb-3">
                         <Col>
-                            <p>No two skin complexions are the same.</p>
+                            <h1>out of your skin.</h1>
                         </Col>
                     </Row>
-                </div>
+                    <div className="paragraph pl-3 mb-3">
+                        <Row>
+                            <Col>
+                                <p>Every single treatment we offer is tailored to your individual needs.</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p>No two skin complexions are the same.</p>
+                            </Col>
+                        </Row>
+                    </div>
+                </Col>
             </motion.div>
         </Container >
     )
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Button } from "react-bootstrap";
+import { Navbar, Nav, Button, Image, Row, Col } from "react-bootstrap";
 import Filter from "./Filter"
 
 class Navigation extends React.Component {
@@ -28,46 +28,49 @@ class Navigation extends React.Component {
         return (
             <div>
                 <Filter className={this.state.isMenuOpen ? "d-block" : "d-none"} />
+
                 <div className={`nav__menu d-block d-lg-none ${isOpen}`}>
                     <Nav className="py-3 d-flex flex-column align-items-center justify-content-center">
                         <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#treatments">Treatments</Nav.Link>
                         <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
                     </Nav>
                 </div>
+
                 <Navbar variant="dark" id="navbar" className="d-flex justify-content-between">
-                    <div className="d-none d-lg-block nav__section">
-                        <Nav className="nav__section">
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="#treatments">Treatments</Nav.Link>
-                            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-                            <Nav.Link href="#contact">Contact</Nav.Link>
-                        </Nav>
-                    </div>
-
-                    <Navbar.Brand href="#home" className="nav__section d-flex justify-content-center">
-                        <img
-                            src={require("./images/logo_fff.png")}
-                            width="300"
-                            height="123"
-                            className="d-inline-block align-top"
-                            alt="Caley Mansfield Skin Revival logo"
-                        />
-                    </Navbar.Brand>
-
-                    <div className="d-none d-lg-block nav__section">
-                        <Nav className="flex-column">
-                            <Nav.Link href="https://www.google.com/maps/place/96+Marlow+Bottom,+Marlow+SL7+3PH/" target="_blank"><i className="fas fa-map-marker-alt"></i>{this.state.address}</Nav.Link>
-                            <Nav.Link href={`mailto:${this.state.address}`}><i className="far fa-envelope"></i>{this.state.email}</Nav.Link>
-                            <Nav.Link href={`tel:${this.state.number}`}><i className="fas fa-phone-alt"></i>{this.state.number}</Nav.Link>
-                        </Nav>
-                    </div>
-
+                    <Col>
+                        <div className="d-none d-lg-block nav__section">
+                            <Nav className="nav__section">
+                                <Nav.Link href="#about">About</Nav.Link>
+                                <Nav.Link href="#treatments">Treatments</Nav.Link>
+                                <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+                            </Nav>
+                        </div>
+                    </Col>
+                    <Col md={6} lg={4}>
+                        <Navbar.Brand href="#home" className="nav__section d-flex justify-content-center">
+                            <Image
+                                src={require("./images/logo_fff.png")}
+                                className="d-inline-block align-top"
+                                alt="Caley Mansfield Skin Revival logo"
+                                fluid
+                            />
+                        </Navbar.Brand>
+                    </Col>
+                    <Col>
+                        <div className="d-none d-lg-block nav__section">
+                            <Nav className="flex-column">
+                                <Nav.Link href="https://www.google.com/maps/place/96+Marlow+Bottom,+Marlow+SL7+3PH/" target="_blank"><i className="fas fa-map-marker-alt"></i>{this.state.address}</Nav.Link>
+                                <Nav.Link href={`mailto:${this.state.address}`}><i className="far fa-envelope"></i>{this.state.email}</Nav.Link>
+                                <Nav.Link href={`tel:${this.state.number}`}><i className="fas fa-phone-alt"></i>{this.state.number}</Nav.Link>
+                            </Nav>
+                        </div>
+                    </Col>
                     <div className="nav__menu-btn d-block d-lg-none">
                         <Button onClick={this.toggleMenu} variant="outline-light" className="menu-btn"><i className="fas fa-bars"></i></Button>
                     </div>
                 </Navbar >
+
             </div>
         )
     }
