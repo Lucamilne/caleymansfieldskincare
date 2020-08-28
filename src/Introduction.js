798fc018bda86ef3aa7d4639a7afeedf7b809533
lucamilne <lucamilne@googlemail.com>
2020-08-28 10:34:27 +0100

    Introduction tweaked and reworked

--- src/Introduction.js
+++ src/Introduction.js
@@ -6,38 +6,25 @@ import { motion } from 'framer-motion'
 const Introduction = () => {
     const [ref, inView] = useInView({
         // rootMargin: '-200px',
-        threshold: 0.8
+        threshold: 0.4
     })
 
     return (
-        <Container id="introduction" className="d-flex align-items-center my-5">
-            <motion.div ref={ref} animate={inView ? { opacity: [0, 1] } : { opacity: 0 }} transition={{ duration: 0.5 }}>
-                <Col>
-                    <Row>
-                        <Col>
-                            <h1>Get the best</h1>
-                        </Col>
-                    </Row>
-                    <Row className="mb-3">
-                        <Col>
-                            <h1>out of your skin.</h1>
-                        </Col>
-                    </Row>
-                    <div className="paragraph pl-3 mb-3">
-                        <Row>
-                            <Col>
-                                <p>Every single treatment we offer is tailored to your individual needs.</p>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col>
-                                <p>No two skin complexions are the same.</p>
-                            </Col>
-                        </Row>
-                    </div>
-                </Col>
-            </motion.div>
-        </Container >
+        <motion.div ref={ref} animate={inView ? { opacity: [0, 1] } : { opacity: 0 }} transition={{ duration: 0.5 }}>
+            <Container fluid id="introduction" className="d-flex align-items-center">
+                <Row>
+                    <Col lg className="column my-5">
+                        <Image fluid src={require("./images/10.png")} />
+                    </Col>
+                    <Col lg className="column mb-5">
+                        <div className="paragraph">
+                            <h1 className="mb-3">Get the best out of your skin.</h1>
+                            <p className="pl-3 mb-3">Every single treatment we offer is tailored to your individual needs. No two skin complexions are the same.</p>
+                        </div>
+                    </Col>
+                </Row>
+            </Container >
+        </motion.div>
     )
 }
 
