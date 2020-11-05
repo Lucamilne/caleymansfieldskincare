190e0605c6c9d140accaa064366f5b49258d7a47
lucamilne <lucamilne@googlemail.com>
2020-11-05 18:36:18 +0000

    logo update

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navbar, Nav, Button, Image, Col } from "react-bootstrap";
+import { Navbar, Nav, Button, Image, Row, Col } from "react-bootstrap";
 import Filter from "./Filter"
 
 class Navigation extends React.Component {
@@ -26,47 +26,41 @@ class Navigation extends React.Component {
         return (
             <div>
                 <Filter className={this.state.isMenuOpen ? "d-block" : "d-none"} />
-
-                <div className={`nav__menu d-block d-lg-none ${isOpen}`}>
+                <div className={`nav__menu d-block d-xl-none ${isOpen}`}>
                     <Nav className="py-3 d-flex flex-column align-items-center justify-content-center">
                         <Nav.Link href="#about">About</Nav.Link>
                         <Nav.Link href="#treatments">Treatments</Nav.Link>
                         <Nav.Link href="#testimonials">Testimonials</Nav.Link>
                     </Nav>
                 </div>
-
-                <Navbar variant="dark" id="navbar" className="d-flex justify-content-between">
-                    <Col>
-                        <div className="d-none d-lg-block nav__section">
-                            <Nav className="nav__section">
-                                <Nav.Link href="#about">About</Nav.Link>
-                                <Nav.Link href="#treatments">Treatments</Nav.Link>
-                                <Nav.Link href="#testimonials">Testimonials</Nav.Link>
-                            </Nav>
-                        </div>
+                <Navbar variant="dark" id="navbar" className="d-flex justify-content-center">
+                    <div className="nav__menu-btn d-none d-md-block d-xl-none">
+                        <Button onClick={this.toggleMenu} variant="outline-light" className="menu-btn"><i className="fas fa-bars"></i></Button>
+                    </div>
+                    <Col xl={4} className="d-none d-xl-block">
+                        <Nav className="nav__section">
+                            <Nav.Link href="#about">About</Nav.Link>
+                            <Nav.Link href="#treatments">Treatments</Nav.Link>
+                            <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+                        </Nav>
                     </Col>
-                    <Col md={6} lg={4}>
-                        <Navbar.Brand href="#home" className="nav__section d-flex justify-content-center">
+                    <Col sm={9} lg={6} xl={4} className="logo">
+                        <Navbar.Brand href="#home" className="m-0">
                             <Image
                                 src={require("./images/logo-alt_fff.png")}
-                                className="d-inline-block align-top"
+                                className="d-inline-block align-top my-3"
                                 alt="Caley Mansfield Skin Revival logo"
                                 fluid
                             />
                         </Navbar.Brand>
                     </Col>
-                    <Col>
-                        <div className="d-none d-lg-block nav__section">
-                            <Nav className="flex-column">
-                                <Nav.Link href="https://www.google.com/maps/place/96+Marlow+Bottom,+Marlow+SL7+3PH/" target="_blank"><i className="fas fa-map-marker-alt"></i>{this.state.address}</Nav.Link>
-                                <Nav.Link href={`mailto:${this.state.address}`}><i className="far fa-envelope"></i>{this.state.email}</Nav.Link>
-                                <Nav.Link href={`tel:${this.state.number}`}><i className="fas fa-phone-alt"></i>{this.state.number}</Nav.Link>
-                            </Nav>
-                        </div>
+                    <Col xl={4} className="d-none d-xl-block">
+                        <Nav className="flex-column">
+                            <Nav.Link href="https://www.google.com/maps/place/96+Marlow+Bottom,+Marlow+SL7+3PH/" target="_blank"><i className="fas fa-map-marker-alt"></i>{this.state.address}</Nav.Link>
+                            <Nav.Link href={`mailto:${this.state.address}`}><i className="far fa-envelope"></i>{this.state.email}</Nav.Link>
+                            <Nav.Link href={`tel:${this.state.number}`}><i className="fas fa-phone-alt"></i>{this.state.number}</Nav.Link>
+                        </Nav>
                     </Col>
-                    <div className="nav__menu-btn d-block d-lg-none">
-                        <Button onClick={this.toggleMenu} variant="outline-light" className="menu-btn"><i className="fas fa-bars"></i></Button>
-                    </div>
                 </Navbar >
 
             </div>
