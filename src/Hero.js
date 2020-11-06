22ee6362764ed209554dd52dd2865e326cd68d31
lucamilne <lucamilne@googlemail.com>
2020-11-06 13:54:05 +0000

    hero video

--- src/Hero.js
+++ src/Hero.js
@@ -7,12 +7,9 @@ class Hero extends React.Component {
             <section className="position-relative" id="hero">
                 <img src={require("./images/mobile-hero.jpg")} alt="Woman undergoing skin care facial treatment" className="d-md-block d-lg-none" />
                 <video autoPlay muted loop className="d-none d-lg-block">
-                    <source src={require("./images/routine2.mp4")} type="video/mp4" />
+                    <source src={require("./images/hero_crop.mp4")} type="video/mp4" />
                 </video>
                 <div className="gradient-filter d-flex align-items-end">
-                    {/* <div className="hero__text position-absolute">
-                        
-                    </div> */}
                 </div>
                 <ShapeFill alt="bottom" color="#eee"/>
             </section>
