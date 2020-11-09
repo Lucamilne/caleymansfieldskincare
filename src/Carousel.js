547d19aff6804c73403020028e84597b5b140b5e
lucamilne <lucamilne@googlemail.com>
2020-11-09 12:11:10 +0000

    Colours changed on Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,9 +1,7 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import SwiperCore, { Navigation, Pagination } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Card, Button } from "react-bootstrap"
-
-//npm isntall node-sass
 import 'swiper/swiper.scss';
 import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
@@ -50,20 +48,48 @@ const products = [
 // install Swiper components
 SwiperCore.use([Navigation, Pagination]);
 
-export default () => {
+function useWindowSize() {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+  
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+      
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+      
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+      
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+  
+    return windowSize;
+  }
+
+function Carousel() {
+    const size = useWindowSize();
+
     return (
         <section id="carousel">
             <Swiper
-                slidesPerView={2}
+                slidesPerView={size.width < 576 ? 1 : 2}
                 spaceBetween={16}
                 grabCursor={true}
                 navigation
                 centeredSlides={true}
                 pagination={{ clickable: true }}
-                onSwiper={(swiper) => console.log(swiper)}
-                onSlideChange={() => console.log('slide change')}
             >
-                {/* <SwiperSlide><img src={require("./images/products/Image01.jpg")} alt="Skin Care Products"></img></SwiperSlide> */}
                 {products.map((product, index) => (
                     <SwiperSlide key={index}>
                         <Card>
@@ -74,7 +100,7 @@ export default () => {
                                     Some quick example text to build on the card title and make up the bulk of
                                     the card's content.
                                 </Card.Text>
-                                <Button variant="primary">Shop</Button>
+                                <Button variant="outline-dark">Shop</Button>
                             </Card.Body>
                         </Card>
                     </SwiperSlide>
@@ -85,3 +111,5 @@ export default () => {
         </section>
     );
 };
+
+export default Carousel;
